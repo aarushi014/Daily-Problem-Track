7730abb201acc3bdad35c5f4ec4df50cc4daebf2
Aarushi Singh <aarushi14aug@gmail.com>
2026-07-04 16:37:40 +0530

    Time: 0 ms (100%), Space: 20.8 MB (95.75%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        if root is None:
+            return None
+        curr = root
+        while curr!=None:
+            if target == curr.val:
+                return curr
+            elif target<curr.val:
+                curr=curr.left
+            else:
+                curr = curr.right
+        return None
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
